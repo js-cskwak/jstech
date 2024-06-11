ea3de2f93c8c3c20915d0e0fc88eb31db8854e92
js-cskwak <cskwak@jstech.com>
2024-06-11 10:39:28 +0900

    update

---
+++ fru_edit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+Manufacturer="JS Technology Inc."
+Name="JG2203A-V4"
+Serial="S202432100004"
+
+ipmitool fru edit 0 field p 0 ${Manufacturer}
+
+sleep 2
+
+ipmitool fru edit 0 field p 1 ${Name}
+
+sleep 2
+
+ipmitool fru edit 0 field p 4 ${Serial}
+
+sleep 2
+
+ipmitool fru print
